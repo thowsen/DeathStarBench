b758f0e57afefbd90eae8ab80d83502cae19565f
Morgan Thowsen <thowsenm@gmail.com>
2023-02-15 16:38:08 +0100

    changed testscript

--- socialNetwork/runtest.sh
+++ socialNetwork/runtest.sh
@@ -2,10 +2,10 @@
 PARAMS=(
     -D 
     exp 
-    -t 4 # threads 
-    -d 1m
-    -c 4 # connections
-    -R 200 # requests per second 
+    -t 10 # threads 
+    -d 30s
+    -c 80    # connections
+    -R 1250 # requests per second 
     -L 
     -s ./wrk2/scripts/social-network/register-user.lua
     http://localhost:8080/wrk2-api/user/register # test to be run
